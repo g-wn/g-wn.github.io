3575a114d97c1bc185cfc0664b2860f7dfc1d241
Gray Nance <graynance@gmail.com>
2023-01-13 10:10:55 -0800

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,69 @@
+export const SITE_TITLE = 'Gray Nance';
+export const SITE_DESCRIPTION =
+  'Gray Nance. Professional Software Engineer base in Los Angeles, California. Proficient in in-demand languages and frameworks such as JavaScript, Python, React, Redux, Express, Flask, SQL, AWS, Docker, and more.';
+
+// ARRAY OF PROJECTS:
+export const PROJECTS = [
+  {
+    name: 'Rungs',
+    description:
+      'Professional networking web application to connect with colleagues, classmates, and friends. Clone of LinkedIn',
+    experiences: [
+      'Utilized Agile Workflow methodologies to successfully deliver ahead of the deadline with 30% more completed features than required.',
+      'Incorporated AWS S3 across the application to efficiently upload and serve images to/from cloud storage, reduce server load, and improve application scalability.',
+      'Researched and implemented WebSockets protocol using Socket.IO on the frontend and Flask-Socket.IO on the backend to allow users to easily and effectively receive notifications and interact via real-time messaging.'
+    ],
+    technologies: [
+      'React/Redux',
+      'JavaScript',
+      'Python',
+      'Flask',
+      'SQLAlchemy',
+      'PostgreSQL',
+      'AWS S3',
+      'Docker',
+      'SQLite3',
+      'HTML',
+      'CSS'
+    ],
+    githubLink: 'https://github.com/g-wn/rungs',
+    liveLink: 'https://rungs.herokuapp.com',
+    photo: 'https://cdn.discordapp.com/attachments/1021558885598900225/1063227056663568455/rungs.jpg'
+  },
+  {
+    name: 'Reduncrate',
+    description:
+      'Curated e-commerce web application allowing users to browse, search, filter, and "purchase" items. Clone of Uncrate',
+    experiences: [
+      'Coordinated backend development by spearheading API route design to be used in the development of the entire app, allowing smooth integration by utilizing RESTful conventions.',
+      'Engineered dynamic and reusable data entry forms by effectively utilizing React State, allowing easy, clear, and efficient recycling of code to appropriately follow DRY coding principles.'
+    ],
+    technologies: [
+      'React/Redux',
+      'JavaScript',
+      'Python',
+      'Flask',
+      'SQLAlchemy',
+      'PostgreSQL',
+      'SQLite3',
+      'HTML',
+      'CSS'
+    ],
+    githubLink: 'https://github.com/g-wn/uncrate-clone',
+    liveLink: 'https://reduncrate.herokuapp.com/',
+    photo: 'https://cdn.discordapp.com/attachments/1021558885598900225/1063227358204670033/reduncrate.jpg'
+  },
+  {
+    name: 'NoCleaningFees',
+    description:
+      'Vacation booking web application allowing users to browse, search, filter, and "book" spots. Clone of Airbnb',
+    experiences: [
+      'Leveraged React Context to enable dynamic modal display throughout the entire site, allowing a significantly improved and intuitive user experience.',
+      'Ensured user privacy through robust validation with React Router and using Bcrypt salting/hashing, protecting user passwords, and guarding against unauthorized changes to a user’s profile or any other restricted data.'
+    ],
+    technologies: ['React/Redux', 'JavaScript', 'Node', 'Express', 'Sequelize', 'PostgreSQL', 'SQLite3', 'HTML', 'CSS'],
+    githubLink: 'https://github.com/g-wn/no-cleaning-fees',
+    liveLink: 'https://nocleaningfees.herokuapp.com/',
+    photo: 'https://cdn.discordapp.com/attachments/1021558885598900225/1063227463787884604/nocleaningfees.jpg'
+  }
+];
